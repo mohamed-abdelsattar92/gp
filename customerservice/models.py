cf90b745ab363867f5df5295447953d324702313
Mohamed Abdel Sattar <mohamed.abdelsattar92@hotmail.com>
2016-06-29 13:42:25 +0200

    Replacing old address city, area, etc.. with the new address_formatted string, longitude and latitude

--- customerservice/models.py
+++ customerservice/models.py
@@ -4,18 +4,18 @@ from django.core.validators import RegexValidator
 
 class Customer(models.Model):
     first_name = models.CharField(max_length = 120)
+    middle_name = models.CharField(max_length = 120)
     last_name = models.CharField(max_length = 120)
     mobile_number = models.CharField(max_length = 11, unique = True, blank = True,\
                                      validators = [RegexValidator(r'\d{11}',"Please enter a valid mobile number.")])
     land_phone_number = models.CharField(max_length = 8, unique = True,\
                                     validators = [RegexValidator(r'\d{8}',"Please enter a valid phone number.")])
-    city = models.CharField(max_length = 50)
-    area = models.CharField(max_length = 50)
-    street = models.CharField(max_length = 50)
-    block_number = models.SmallIntegerField()
+    address_formated = models.CharField(max_length = 200)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
 
     def __str__(self):
-        return self.first_name + "\t" + self.last_name
+        return self.first_name + "\t" + self.middle_name + "\t" + self.last_name
 
 
 class Device(models.Model):
@@ -26,7 +26,4 @@ class Device(models.Model):
     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
 
     def __str__(self):
-        return self.model_name + " " + self.customer.__str__()
-
-    def get_customer_name(self):
-        return self.customer.first_name + " " + self.customer.last_name
+        return str(self.serial_number) + " " + self.customer.__str__()
