20f056659e21c2e4bbaf9da3f11b42ab7ba5d79b
Mohamed Abdel Sattar <mohamed.abdelsattar92@hotmail.com>
2016-07-07 22:04:01 +0200

    Modefying the technician model adding the middle name and location longitude and latitude

--- technician/models.py
+++ technician/models.py
@@ -6,18 +6,21 @@ from customerservice.models import Device
 
 class Technician(models.Model):
     first_name = models.CharField(max_length = 120)
+    middle_name = models.CharField(max_length = 120)
     last_name = models.CharField(max_length = 120)
     city = models.CharField(max_length = 50)
     area = models.CharField(max_length = 50)
     street = models.CharField(max_length = 50)
     block_number = models.SmallIntegerField()
+    location_longitude = models.FloatField(blank = True, null = True)
+    location_latitude = models.FloatField(blank = True, null = True)
     mobile_number = models.CharField(max_length = 11, unique = True, blank = True,\
                                      validators = [RegexValidator(r'\d{11}',"Please enter a valid mobile number.")])
     land_phone_number = models.CharField(max_length = 8, unique = True,\
                                     validators = [RegexValidator(r'\d{8}',"Please enter a valid phone number.")])
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.first_name + " " + self.middle_name + " " + self.last_name
 
 
 class Skill(models.Model):
